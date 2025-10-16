2a8b935d098fc91861e26452abb9356887a8f81e
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2025-10-16 10:38:53 +0200

    2.1 - 2.2 : add api route, add filter by houses

---
+++ script.js
@@ -0,0 +1,49 @@
+let charactersList = document.querySelector(".characters");
+let housesList = document.querySelector(".houses");
+
+const allData = await fetch(`https://hp-api.onrender.com/api/characters`)
+  .then((response) => response.json())
+  .catch((error) => alert("Erreur : " + error));
+
+const data = allData.slice(0, 12);
+const housesNamesList = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+
+const filterByHouse = (house) => {
+  let filteredList = data.filter((character) => character.house === house);
+  console.log(filteredList, house);
+
+  return filteredList;
+};
+
+const renderCharactersList = (data) => {
+  if (housesList.childElementCount === 0) {
+    housesNamesList.forEach((house) => {
+      let houseButton = document.createElement("button");
+      houseButton.innerHTML = `
+      <div><img src="./images/logo/${house.toUpperCase()}.png" alt="" /></div>
+      `;
+
+      housesList.appendChild(houseButton);
+
+      houseButton.addEventListener("click", () => {
+        renderCharactersList(filterByHouse(house));
+      });
+    });
+  }
+
+  charactersList.innerHTML = "";
+
+  data.forEach((character) => {
+    let characterDiv = document.createElement("article");
+    characterDiv.innerHTML = `
+        <div class=${character.house.toLowerCase()}>
+            <img src="${character.image}" alt="${character.name}" />
+            <p>${character.name}</p>
+          </div>
+        `;
+
+    charactersList.appendChild(characterDiv);
+  });
+};
+
+renderCharactersList(data);
