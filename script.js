eff6d2575b733b847074fd1ddee89787ce32b626
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2025-10-16 11:42:56 +0200

    add dead, alive, alphabet filter

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let charactersList = document.querySelector(".characters");
 let housesList = document.querySelector(".houses");
+let filtresList = document.querySelector(".filtres");
 
 const allData = await fetch(`https://hp-api.onrender.com/api/characters`)
   .then((response) => response.json())
@@ -8,14 +9,58 @@ const allData = await fetch(`https://hp-api.onrender.com/api/characters`)
 const data = allData.slice(0, 12);
 const housesNamesList = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 
-
 const filterByHouse = (house) => {
   let filteredList = data.filter((character) => character.house === house);
 
   return filteredList;
 };
 
+const filterByAlphabeticalOrder = () => {
+  let filteredList = data.sort((a, b) => a.name.localeCompare(b.name));
+  return filteredList;
+};
+
+const filterByAlive = () => {
+  let filteredList = data.filter((character) => character.alive === true);
+  return filteredList;
+};
+
+const filterByDead= () => {
+  let filteredList = data.filter((character) => character.alive === false);
+  return filteredList;
+};
+console.log(data)
+
 const renderCharactersList = (data) => {
+  if (filtresList.childElementCount === 0) {
+    let alphabetButton = document.createElement("button");
+
+    let aliveButton = document.createElement("button");
+    let deadButton = document.createElement("button");
+
+    alphabetButton.textContent = "Alphabetical";
+
+    aliveButton.textContent = "Alive";
+    deadButton.textContent = "Dead";
+
+    filtresList.appendChild(alphabetButton);
+
+    filtresList.appendChild(aliveButton);
+    filtresList.appendChild(deadButton);
+
+    alphabetButton.addEventListener("click", () => {
+      renderCharactersList(filterByAlphabeticalOrder());
+    });
+
+    aliveButton.addEventListener("click", () => {
+      renderCharactersList(filterByAlive());
+    });
+
+    deadButton.addEventListener("click", () => {
+      renderCharactersList(filterByDead());
+    });
+  }
+
   if (housesList.childElementCount === 0) {
     housesNamesList.forEach((house) => {
       let houseButton = document.createElement("button");
@@ -36,7 +81,9 @@ const renderCharactersList = (data) => {
   data.forEach((character) => {
     let characterDiv = document.createElement("article");
     characterDiv.innerHTML = `
-        <a href="./details.html?id=${character.id}" class=${character.house.toLowerCase()}>
+        <a href="./details.html?id=${
+          character.id
+        }" class=${character.house.toLowerCase()}>
             <img src="${character.image}" alt="${character.name}" />
             <p>${character.name}</p>
           </a>
